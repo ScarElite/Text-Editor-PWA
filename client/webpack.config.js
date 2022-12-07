3d95af0611dfbec209a0569d657784d6a3bce718
Mitchell Armstrong <mra0211@gmail.com>
2022-12-06 22:59:04 -0500

    Created the install functionality

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
-const { GenerateSW } = require("workbox-webpack-plugin");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -22,7 +22,10 @@ module.exports = () => {
         template: "./index.html",
         title: "Webpack Plugin",
       }),
-      new GenerateSW(),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
       new WebpackPwaManifest({
         name: "Text Editor PWA",
         short_name: "Text Editor",
@@ -37,22 +40,12 @@ module.exports = () => {
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join("assets", "icons"),
           },
-          {
-            src: path.resolve("src/images/logo.png"),
-            size: "1024x1024",
-            destination: path.join("assets", "icons"),
-            purpose: "maskable",
-          },
         ],
       }),
     ],
 
     module: {
       rules: [
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: "asset/resource",
-        },
         {
           test: /\.css$/i,
           use: ["style-loader", "css-loader"],
